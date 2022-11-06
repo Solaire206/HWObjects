a737c4e75afc5b63f42ca48661328734da98b1e8
Petr <petiauzunov1997481@gmail.com>
2022-11-06 16:22:23 +0400

    HW. Objects

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+     String author;
+    int year;
+
+
+    public Book(String author, int year) {
+        this.author = author;
+        this.year = year;
+    }
+    public String   getAuthor(){
+        return this.author;
+    }
+    public int getYear(){
+        return  this.year;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+
 }
